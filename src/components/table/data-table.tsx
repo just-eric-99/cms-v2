d1182ef1be05d08b38c8a64e8bb8f3ebbccb7d0d
Eric Low <eric@Erics-MacBook-Pro.local>
2024-06-13 21:15:16 +0800

    added filter

--- src/components/table/data-table.tsx
+++ src/components/table/data-table.tsx
@@ -23,6 +23,10 @@ import {
 // import { Input } from '@/sortable/ui/input'
 import { useNavigate } from 'react-router'
 import { DataTablePagination } from '@/components/table/data-table-pagination.tsx'
+import { Input } from '@/components/ui/input.tsx'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import { StringOrTemplateHeader } from '@tanstack/table-core/src/types.ts'
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -57,7 +61,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <>
-      <div className='flex flex-row-reverse items-center justify-between pb-4'>
+      <div className='flex flex-row items-center justify-between gap-4 pb-4'>
         {/* todo: to parse in to filter field here */}
         {/*<Input*/}
         {/*  placeholder='Filter'*/}
@@ -67,6 +71,30 @@ export function DataTable<TData, TValue>({
         {/*  }*/}
         {/*  className='max-w-sm'*/}
         {/*/>*/}
+
+        {table.getAllColumns().map((columnFilter) => {
+          const getHeader = (header: StringOrTemplateHeader) => {
+            if (typeof header === 'function')
+              return header(columnFilter.columnDef)
+            return header
+          }
+
+          const child = getHeader(columnFilter.columnDef.header).props.children
+          const placeholder = typeof child === 'string' ? child : child[0]
+
+          return (
+            <Input
+              key={columnFilter.columnDef.id}
+              placeholder={`Filter ${placeholder}`}
+              value={columnFilter.getFilterValue() as string}
+              onChange={(event) =>
+                columnFilter.setFilterValue(event.target.value)
+              }
+              className='max-w-sm'
+            />
+          )
+        })}
+        <div className={"flex-1"}/>
         {createComponent}
       </div>
       <div className='rounded-md border'>
